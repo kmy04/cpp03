b0f38b5161adfc7116370192b168fb1dcde9cd61
minyekim <biie2004@naver.com>
2024-09-15 14:19:29 +0900

    Add : ScavTrap 상속

---
+++ ex01/main.cpp
@@ -0,0 +1,29 @@
+#include "ScavTrap.hpp"
+
+int main() {
+    // virtual 키워드 테스트
+	std::cout << "virtual 키워드 테스트" << std::endl;
+	ClapTrap* ptr = new ScavTrap("Scavvy");
+	delete ptr;  // ScavTrap 소멸자 -> ClapTrap 소멸자 호출
+	std::cout << std::endl;
+
+
+    std::cout << "ScavTrap 기본 생성자 및 이름을 받는 생성자 테스트" << std::endl;
+    ScavTrap scav1;
+    ScavTrap scav2("Scavvy");
+	std::cout << std::endl;
+    
+    std::cout << "ScavTrap 복사 생성자 및 복사 대입 연산자 테스트" << std::endl;
+    ScavTrap scav3(scav2);
+    scav1 = scav2;
+	std::cout << std::endl;
+    
+    std::cout << "기능 테스트" << std::endl;
+    scav2.attack("target");
+    scav2.guardGate();
+	std::cout << std::endl;
+
+	std::cout << "소멸자 테스트" << std::endl;
+    
+    return 0;
+}
